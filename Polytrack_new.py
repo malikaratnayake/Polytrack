corrected detection processing and iou calculation

Polytrack_new.py
@@ -4,13 +4,11 @@ import logging
 import time
 from datetime import datetime
 from pathlib import Path
-from queue import Empty, Queue
-from threading import Event, Thread
 from itertools import product
 import cv2
 import numpy as np
-from ultralytics import YOLO
-from polytrack.InsectTracker import InsectTracker
+# from ultralytics import YOLO
+from polytrack.InsectTracke0 import InsectTracker
 from polytrack.InsectRecorder0 import Recorder
 from polytrack.EventLogger import EventLogger
 from polytrack.FlowerTracker import FlowerTracker
@@ -31,7 +29,7 @@ class Config:
         tracking_insects: list,
         output_video_dimensions: int,
         input_video_dimensions: int,
-        insect_detector: YOLO,
+        insect_detector: str,
         insect_iou_threshold: float,
         dl_detection_confidence: float,
         min_blob_area: int,
@@ -43,18 +41,22 @@ class Config:
         max_interframe_travel: int,
         info_filename: str,
         iou_threshold: float,
-        model_insects_large: YOLO,
+        model_insects_large: str,
         edge_pixels: int,
         show_video_output: bool,
         save_video_output: bool,
         video_codec: str,
         framerate: int,
         prediction_method: str,
-        flower_detector: YOLO,
+        flower_detector: str,
         flower_iou_threshold: float,
         flower_detection_confidence: float,
         flower_classes: np.ndarray,
-        flower_border: int
+        flower_border: int,
+        tracking_insect_classes: np.ndarray,
+        track_flowers: bool,
+        additional_new_insect_verification: bool,
+        additional_new_insect_verification_confidence: list
 
     ) -> None:
 
@@ -89,6 +91,10 @@ class Config:
         self.flower_detection_confidence = flower_detection_confidence
         self.flower_classes = flower_classes
         self.flower_border = flower_border
+        self.tracking_insect_classes = tracking_insect_classes
+        self.track_flowers = track_flowers
+        self.additional_new_insect_verification = additional_new_insect_verification
+        self.additional_new_insect_verification_confidence = additional_new_insect_verification_confidence
 
 
 
@@ -142,13 +148,13 @@ class TracknRecord():
                 for_predictions = self.RecordTracks.record_track(frame, nframe, mapped_frame_num,fgbg_associated_detections, dl_associated_detections, missing_insects, new_insects)
                 predicted_position = self.TrackInsects.predict_next(for_predictions)
 
-                if nframe in self.full_frame_numbers:
+                if (nframe in self.full_frame_numbers) and self.TrackFlowers is not None:
                     associated_flower_detections, missing_flowers, new_flower_detections = self.TrackFlowers.run_flower_tracker(frame, flower_predictions)
                     flower_detections_for_predictions, latest_flower_positions = self.RecordFlowers.record_flowers(mapped_frame_num, associated_flower_detections, missing_flowers, new_flower_detections)
                     flower_predictions = self.TrackFlowers.predict_next(flower_detections_for_predictions)
                     self.RecordTracks.update_flower_positions(latest_flower_positions, self.RecordFlowers.flower_border)
 
-                if len(for_predictions) > 0:
+                if (len(for_predictions) > 0) and self.RecordFlowers is not None:
                     insect_flower_visits = self.RecordFlowers.monitor_flower_visits(for_predictions)
                     self.RecordFlowers.record_flower_visitations(insect_flower_visits, mapped_frame_num, self.RecordTracks.insect_tracks)
                     
@@ -157,8 +163,9 @@ class TracknRecord():
 
             else:
                 LOGGER.info("Finished processing video. Exiting...")
-                self.RecordFlowers.save_flower_tracks()
                 self.RecordTracks.save_inprogress_tracks(predicted_position)
+                if self.RecordFlowers is not None: self.RecordFlowers.save_flower_tracks()
+                
                 break
 
         self.vid.release()
@@ -227,7 +234,10 @@ def main(config: Config):
         info_filename = config.info_filename,
         iou_threshold = config.iou_threshold,
         model_insects_large = config.model_insects_large,
-        prediction_method = config.prediction_method)
+        prediction_method = config.prediction_method,
+        tracking_insect_classes = config.tracking_insect_classes,
+        additional_new_insect_verification= config.additional_new_insect_verification,
+        additional_new_insect_verification_confidence= config.additional_new_insect_verification_confidence)
     
     record_tracks = Recorder(
         input_video_dimensions = config.input_video_dimensions,
@@ -243,24 +253,26 @@ def main(config: Config):
         tracking_insects = config.tracking_insects,
         edge_pixels = config.edge_pixels)
     
-    track_flowers = FlowerTracker(
-        flower_detector = config.flower_detector,
-        flower_iou_threshold = config.flower_iou_threshold,
-        flower_detection_confidence = config.flower_detection_confidence,
-        flower_classes = config.flower_classes)
-    
-    record_flowers = FlowerRecorder(
-        output_directory = output_directory,
-        flower_border = config.flower_border)
+    if config.track_flowers:
+        track_flowers = FlowerTracker(
+            flower_detector = config.flower_detector,
+            flower_iou_threshold = config.flower_iou_threshold,
+            flower_detection_confidence = config.flower_detection_confidence,
+            flower_classes = config.flower_classes)
+        
+        record_flowers = FlowerRecorder(
+            output_directory = output_directory,
+            flower_border = config.flower_border)
     
     track_and_record = TracknRecord(
         video_source = config.video_source,
         RecordTracks = record_tracks,
         TrackInsects = track_insects,
-        TrackFlowers = track_flowers,
-        RecordFlowers = record_flowers,
+        TrackFlowers = track_flowers if config.track_flowers else None,
+        RecordFlowers = record_flowers if config.track_flowers else None,
         compressed_video = config.compressed_video,
         info_filename = config.info_filename)
+    
     
     # Run the TracknRecord instance
     frames_processed = track_and_record.run()
@@ -343,6 +355,10 @@ if __name__ == "__main__":
     flower_detection_confidence = CONFIG.flower_detection_confidence
     flower_classes = CONFIG.flower_classes
     flower_border = CONFIG.flower_border
+    tracking_insect_classes = CONFIG.tracking_insect_classes
+    track_flowers = CONFIG.track_flowers
+    additional_new_insect_verification = CONFIG.additional_new_insect_verification
+    additional_new_insect_verification_confidence = CONFIG.additional_new_insect_verification_confidence
     
 
     video_source = Path(video_source)
@@ -403,7 +419,11 @@ if __name__ == "__main__":
                 "flower_iou_threshold" : CONFIG.flower_iou_threshold,
                 "flower_detection_confidence": CONFIG.flower_detection_confidence,
                 "flower_classes" : CONFIG.flower_classes,
-                "flower_border" : CONFIG.flower_border
+                "flower_border" : CONFIG.flower_border,
+                "tracking_insect_classes" : CONFIG.tracking_insect_classes,
+                "track_flowers" : CONFIG.track_flowers,
+                "additional_new_insect_verification": CONFIG.additional_new_insect_verification,
+                "additional_new_insect_verification_confidence": CONFIG.additional_new_insect_verification_confidence
             }
             
         )

polytrack/FlowerTracker.py
@@ -9,7 +9,7 @@ LOGGER = logging.getLogger()
 class DL_Flower_Detector():
 
     def __init__(self,
-                flower_detector: YOLO,
+                flower_detector: str,
                 flower_iou_threshold: float,
                 flower_detection_confidence: float,
                 flower_classes: np.ndarray) -> None:
@@ -86,13 +86,13 @@ class DL_Flower_Detector():
 class FlowerTracker(DL_Flower_Detector, TrackingMethods):
 
     def __init__(self,
-                flower_detector: YOLO,
+                flower_detector: str,
                 flower_iou_threshold: float,
                 flower_detection_confidence: float,
                 flower_classes: np.ndarray) -> None:
         
         TrackingMethods.__init__(self,
-                                 prediction_method = 'ConstantVelocity')
+                                 prediction_method = 'ExtendedKalman')
         
         DL_Flower_Detector.__init__(self,
                              flower_detector = flower_detector,

polytrack/test.py
@@ -1,71 +1,241 @@
-import pandas as pd
+from datetime import datetime
+import glob
 import math
+import threading
+from time import time
+import numpy as np
+from ultralytics import YOLO
+import os
+import shutil
+import sys
+import cv2
+from PIL import Image
 
-def is_point_inside_circle(x0, y0, cx, cy, radius):
-    # Calculate the distance between the point (x0, y0) and the circle's center (cx, cy)
-    distance_to_center = math.sqrt((x0 - cx)**2 + (y0 - cy)**2)
-    
-    # Check if the distance to the center is less than the radius
-    if distance_to_center < radius:
-        return True  # The point is inside the circle
-    else:
-        return False  # The point is outside the circle
+# set the path to the YOLOv5 model
+yolo_path = "./data/yolov8_models/yolo-stingless-best.pt"
 
-def check_insect_flower_relationship(insect_tracks, flower_tracks, N):
-    # Get the last N frames from insect_tracks and flower_tracks
-    last_insect_frames = insect_tracks[insect_tracks['nframe'].isin(range(max(insect_tracks['nframe']) - N + 1, max(insect_tracks['nframe']) + 1))]
-    last_flower_frames = flower_tracks[flower_tracks['nframe'].isin(range(max(flower_tracks['nframe']) - N + 1, max(flower_tracks['nframe']) + 1))]
-    
-    # Initialize a list to store flower numbers for each insect (default to None)
-    flower_numbers = [None] * len(last_insect_frames)
-    
-    # Iterate over each row in the last_insect_frames dataframe
-    for idx, insect_row in last_insect_frames.iterrows():
-        x0 = insect_row['x0']
-        y0 = insect_row['y0']
+# global constants
+ROI_HEIGHT = 500
+ROI_WIDTH = 500
+Resize_height = 256
+Resize_width = 256
+VideoExtension = ".mp4"
+ImageExtension = ".png"
+
+def process_yolo_results(result, class_names, conf_thresh=0.5):
+    """
+    processes the yolo results and returns the frame with the bounding boxes.
+    results: list of results from yolo inference
+    frame: np.array
+    class_names: dict
+    draw_bounding_box: bool
+    return: frame, np.array
+    """
+    box_list = {}
+    for i in range(len(class_names)):
+        box_list[class_names[i]] = []
+    if result is not None and len(result.boxes.cpu().numpy().data)>0:
         
-        # Iterate over each row in the last_flower_frames dataframe
-        for flower_idx, flower_row in last_flower_frames.iterrows():
-            cx = flower_row['cx']
-            cy = flower_row['cy']
-            radius = flower_row['radius']
-            flower_num = flower_row['flower_num']
+        # class_names = {0: 'fly', 1: 'tag'}
+        data = result.boxes.cpu().numpy().data
+        boxes = data[:, 0:4].astype(int)
+        confs = data[:, 4]
+        class_ids = data[:, 5].astype(int)
+        # for each box, draw the bounding box and label
+        for i, box in enumerate(boxes):
+            if confs[i] > conf_thresh and class_ids[i] in class_names:
+                box_list[class_names[class_ids[i]]].append(box)
+
+    return box_list
+
+def draw_bounding_box(frame, box, class_name, color=(0, 105, 255)): # color for ocean blue (0, 105, 255)
+    """
+    Draw the bounding box on the frame
+    frame: np.array
+    box: tuple (x1, y1, x2, y2)
+    class_id: int
+    class_names: dict
+    return: np.array
+    """
+    cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), color, 2)
+    # cv2.putText(frame, class_name, (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
+    return frame
+
+def enque_deque_frame(frame, type='raw', enque=True):
+    """
+    enque or deque the frame from the queue
+    frame: np.array
+    type: str
+    enque: bool
+    return: None
+    """
+    global raw_frame_queue, processed_frame_queue
+    if type == 'raw':
+        if enque:
+            raw_frame_queue.append(frame)
+        else:
+            frame = raw_frame_queue.popleft()
+    elif type == 'processed':
+        if enque:
+            processed_frame_queue.append(frame)
+        else:
+            frame = processed_frame_queue.popleft()
+    return frame
+
+def get_batch_frames(batch_size=16):
+    """
+    get a batch of frames from the queue
+    batch_size: int
+    return: list of np.array
+    """
+    global raw_frame_queue
+    batch_frames = []
+    while len(batch_frames) < batch_size:
+        if len(raw_frame_queue) > 0:
+            frame = enque_deque_frame(None, type='raw', enque=False)
+            batch_frames.append(frame)
+        else:
+            break
+    return batch_frames
+
+def process_video_v2(video_path, yolo, output_video_path):
+    """
+    Process the dronefly video
+    video_path: str
+    yolo: YOLO
+    class_names: dict
+    output_path: str
+    output_video_path: str
+    output_video_fps: int
+    """
+    class_names = {0: 'bee'}
+    # load the video
+    cap = cv2.VideoCapture(video_path)
+    n_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    # get the video's frame rate
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    # get the video's frame dimensions
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    # create a video writer object
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(output_video_path, fourcc, fps, (width, height))
+    t1 = time()
+    frame_batch = []
+    batch_size = 32
+    csv_string = ""
+    frame_ids = []
+
+    # process the video frame by frame
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frame_no = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
+        if len(frame_batch) < batch_size:
+            frame_batch.append(frame)
+            frame_ids.append(frame_no)
+            continue
+        # process the frame
+        results = yolo(frame_batch, verbose=False)
+        frame_counter = 0
+        for result in results:
+            frame = frame_batch[frame_counter]
+            frame_counter += 1
+            box_list = process_yolo_results(result, class_names, conf_thresh=0.4)
+            bee_count = len(box_list['bee'])
             
-            # Check if the center point of the insect is inside the flower's circle
-            if is_point_inside_circle(x0, y0, cx, cy, radius):
-                if flower_numbers[idx] is None:
-                    flower_numbers[idx] = []
-                flower_numbers[idx].append(flower_num)
+            for class_name, boxes in box_list.items():
+                for box in boxes:
+                    frame = draw_bounding_box(frame, box, class_name)
+                    center = (int((box[0] + box[2]) / 2), int((box[1] + box[3]) / 2))
+                    csv_string += f"{frame_ids[frame_counter-1]},{center[0]},{center[1]}\n"
+            # write the frame to the output video
+            t = time() - t1
+            fps = frame_no / t
+            cv2.putText(frame, f"FPS: {fps:0.2f}, Bee Count: {bee_count:2d}", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+            out.write(frame)
+            print(f"Processed frame: {frame_no:5d}/{n_frames:5d} ({frame_no/n_frames:.2%}), fps={fps:0.2f}", end='\r')
+        frame_batch = []
+        frame_ids = []
+    # release the video capture and video writer objects
+    cap.release()
+    # out.release()
+    csv_file = output_video_path.replace(VideoExtension, "_detections.csv")
+    with open(csv_file, 'w') as f:
+        f.write(csv_string)
+
+    print(f"\nProcessed video: {video_path} -> {output_video_path} and {csv_file}")
+
+
+
+def process_video(video_path, yolo, output_video_path):
+    """
+    Process the dronefly video
+    video_path: str
+    yolo: YOLO
+    class_names: dict
+    output_path: str
+    output_video_path: str
+    output_video_fps: int
+    """
+    class_names = {0: 'bee'}
+    # load the video
+    cap = cv2.VideoCapture(video_path)
+    n_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    # get the video's frame rate
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    # get the video's frame dimensions
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    # create a video writer object
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(output_video_path, fourcc, fps, (width, height))
+    t1 = time()
+    # process the video frame by frame
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frame_no = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
+        
+        # process the frame
+        results = yolo(frame, verbose=False)
+        box_list = process_yolo_results(results, class_names, conf_thresh=0.4)
+        bee_count = len(box_list['bee'])
+        for class_name, boxes in box_list.items():
+            for box in boxes:
+                frame = draw_bounding_box(frame, box, class_name)
+        # write the frame to the output video
+        t = time() - t1
+        fps = frame_no / t
+        cv2.putText(frame, f"FPS: {fps:0.2f}, Bee Count: {bee_count:2d}", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+        out.write(frame)
+        print(f"Processed frame: {frame_no:5d}/{n_frames:5d} ({frame_no/n_frames:.2%}), fps={fps:0.2f}", end='\r')
+    # release the video capture and video writer objects
+    cap.release()
+    out.release()
+    print(f"\nProcessed video: {video_path} -> {output_video_path}")
+
+if __name__ == '__main__':
+    # load the YOLO model
+    yolo = YOLO(yolo_path)
+    yolo.to('cuda:0')
+    print("Loaded YOLO model successfully to GPU")
+    # process the dronefly video
+    path= "/Users/mrat0010/Documents/GitHub/Polytrack_WIP/data/video/Camera_43_20240422_113145/Camera_43_20240422_130601.avi"
+
+    video_list = glob.glob(path)
+    print(f"Found {len(video_list)} videos to process")
+    os.makedirs('../../DATASETS/BeeHiveMonitoring', exist_ok=True)
+    for video_path in video_list:
+        print(f"Processing video: {video_path}")
+        file_base_name = os.path.basename(video_path)
+        output_video_path = os.path.join('../../DATASETS/BeeHiveMonitoring', file_base_name)
+        print(f"Output video path: {output_video_path}")
+        # # output_video_path = video_path.replace('./Videos/', './Output/')
+        # if os.path.exists(output_video_path):
+        #     print(f"Output video already exists: {output_video_path}")
+        #     # continue
+        process_video_v2(video_path, yolo, output_video_path)
     
-    # Add a new column 'flower_num' to the last_insect_frames dataframe
-    last_insect_frames['flower_num'] = flower_numbers
-    
-    return last_insect_frames
-
-# Example usage:
-# Assuming insect_tracks and flower_tracks are pandas dataframes containing the specified columns
-
-# Generate example dataframes (replace this with your actual data)
-insect_tracks = pd.DataFrame({
-    'nframe': [1, 1, 2, 2, 3, 3],
-    'insect_num': [1, 2, 3, 4, 5, 6],
-    'x0': [10, 15, 20, 25, 30, 35],
-    'y0': [20, 25, 30, 35, 40, 45]
-})
-
-flower_tracks = pd.DataFrame({
-    'nframe': [1, 1, 2, 2, 3, 3],
-    'flower_num': [101, 102, 103, 104, 105, 106],
-    'cx': [12, 22, 32, 42, 52, 62],
-    'cy': [22, 32, 42, 52, 62, 72],
-    'radius': [5, 8, 7, 10, 6, 9]
-})
-
-# Specify the number of frames (N) to consider
-N = 3
-
-# Check insect-flower relationship for the last N frames
-result_df = check_insect_flower_relationship(insect_tracks, flower_tracks, N)
-
-# Display the updated insect_tracks dataframe with the 'flower_num' column
-print(result_df)